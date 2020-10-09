69fe821c88e1ed071f7c6d5caca77e417f340e11
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-10-09 16:36:26 +0200

    Quadric equation

--- Ex_1/app/src/main/java/com/game/calculate_delta/MainActivity.java
+++ Ex_1/app/src/main/java/com/game/calculate_delta/MainActivity.java
@@ -1,56 +1,70 @@
 package com.game.calculate_delta;
 
-import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
 import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private double firstInputDouble;
+    private double secondInputDouble;
+    private double thirdInputDouble;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Button computeButton = (Button) findViewById(R.id.button);
+        computeButton.setOnClickListener(computeButtonListener);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private View.OnClickListener computeButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            EditText firstInput = (EditText) findViewById(R.id.first_input);
+            EditText secondInput = (EditText) findViewById(R.id.second_input);
+            EditText thirdInput = (EditText) findViewById(R.id.third_input);
+            firstInputDouble = Double.parseDouble(firstInput.getText().toString());
+            secondInputDouble = Double.parseDouble(secondInput.getText().toString());
+            thirdInputDouble = Double.parseDouble(thirdInput.getText().toString());
+            calculateRoots();
         }
+    };
 
-        return super.onOptionsItemSelected(item);
+    private Double calculateDelta(){
+        return secondInputDouble * secondInputDouble - 4 * firstInputDouble * thirdInputDouble;
+    }
+
+    private double calculateRoot(int minus, double delta){
+        return  (-1 * secondInputDouble + minus * Math.sqrt(delta)) / 2 * firstInputDouble;
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void calculateRoots(){
+        TextView output = (TextView) findViewById(R.id.output);
+        if(firstInputDouble == 0.0 ) {
+            output.setText("It is not a quadratic equation.");
+            return;
+        }
+        double delta = calculateDelta();
+        if(delta < 0.0) {
+            output.setText("The quadratic equation hasn't any roots. " + ". The square discriminant equals " + delta);
+            return;
+        }
+        if (delta == 0) {
+            double first_root = calculateRoot(0, delta);
+            output.setText("The quadratic equation has one root equals " + first_root + ". The square discriminant equals " + delta);
+            return;
+        }
+        double first_root = calculateRoot(1, delta);
+        double second_root = calculateRoot(-1, delta);
+        output.setText("The quadratic equation has two roots. First equals " + first_root + ", second equals " + second_root + ". The square discriminant equals " + delta);
+        return;
     }
 }
