40e5034412ea90a384ce1ee586f621a71f095785
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-10-19 09:01:44 +0200

    Workng youtube

--- Ex_5/app/src/main/java/tech/szymanskazdrzalik/youtubeplayer/MainActivity.java
+++ Ex_5/app/src/main/java/tech/szymanskazdrzalik/youtubeplayer/MainActivity.java
@@ -1,14 +1,38 @@
 package tech.szymanskazdrzalik.youtubeplayer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+
+    private YouTubePlayerView youTubePlayerView;
+    private final YouTubePlayer.OnInitializedListener onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+        @Override
+        public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+            youTubePlayer.loadVideo("s0-f5RncxcA");
+        }
+
+        @Override
+        public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        youTubePlayerView = findViewById(R.id.youtube_view);
+
     }
+
+    public void playButtonListener(View v){
+        youTubePlayerView.initialize("", onInitializedListener);
+    }
+
 }
