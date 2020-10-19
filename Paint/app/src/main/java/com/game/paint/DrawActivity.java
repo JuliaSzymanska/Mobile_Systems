ca12c9644955903365ec2d51476508b37970dd7a
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-10-19 15:08:37 +0200

    Added path

--- Paint/app/src/main/java/com/game/paint/DrawActivity.java
+++ Paint/app/src/main/java/com/game/paint/DrawActivity.java
@@ -1,7 +1,11 @@
 package com.game.paint;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.graphics.BlurMaskFilter;
+import android.graphics.Color;
+import android.graphics.EmbossMaskFilter;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,12 +18,27 @@ public class DrawActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw);
+        drawView = findViewById(R.id.drawView);
         drawView.requestFocus();
-        drawView = (DrawView) findViewById(R.id.drawView);
     }
 
     public void blurButtonListener(View v) {
-            this.drawView.paint.setMaskFilter(new BlurMaskFilter(15, BlurMaskFilter.Blur.NORMAL));
-    };
+        this.drawView.paint.setMaskFilter(new BlurMaskFilter(15, BlurMaskFilter.Blur.NORMAL));
+//        this.drawView.eraser();
+    }
+
+    public void embossButtonListener(View v) {
+        EmbossMaskFilter mEmboss = new EmbossMaskFilter(new float[]{1, 1, 1}, 0.5f, 0.6f, 2f);
+        this.drawView.paint.setMaskFilter(mEmboss);
+    }
+
+    public void normalButtonListener(View v) {
+        this.drawView.paint.setMaskFilter(null);
+    }
+
+    public void clearButtonListener(View v) {
+//        this.drawView.paint.setMaskFilter(null);
+//        this.drawView.canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+    }
 
 }
--- Paint/app/src/main/java/com/game/paint/DrawView.java
+++ Paint/app/src/main/java/com/game/paint/DrawView.java
@@ -3,40 +3,96 @@ package com.game.paint;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.MaskFilter;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
+import androidx.annotation.Nullable;
+
 public class DrawView extends View implements View.OnTouchListener {
-    public final Paint paint;
-    private Bitmap bitmap;
-    private Canvas canvas;
+    Paint paint;
+    Bitmap bitmap;
+    Canvas canvas;
+    Path path;
+    MaskFilter blur = new BlurMaskFilter(8, BlurMaskFilter.Blur.NORMAL);
 
     public DrawView(Context context) {
         super(context);
-        paint = new Paint();
-        paint.setColor(Color.RED);
-        paint.setStyle(Paint.Style.STROKE);
-//        EmbossMaskFilter mEmboss = new EmbossMaskFilter(new float[]{1, 1, 1}, 0.5f, 0.6f, 2f);
-//        paint.setMaskFilter(mEmboss);
+        init();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public DrawView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public DrawView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public DrawView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void init() {
         this.setOnTouchListener(this);
         setFocusable(true);
         setFocusableInTouchMode(true);
+        paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setAntiAlias(true);
+        paint.setStrokeWidth(10);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        path = new Path();
     }
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        canvas.drawCircle(event.getX(), event.getY(), 30, paint);
-        invalidate();
+        float pointX = event.getX();
+        float pointY = event.getY();
+        // Checks for the event that occurs
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(pointX, pointY);
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                path.lineTo(pointX, pointY);
+                break;
+            default:
+                return false;
+        }
+        // Force a view to draw again
+        postInvalidate();
         return true;
     }
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawBitmap(bitmap, 0, 0, null);
+//        canvas.drawBitmap(bitmap, 0, 0, null);
+        canvas.drawPath(path, paint);
     }
 
     @Override
