4b49f7ffda0457223f630f0b9800ccc03cd43863
224441 Szymańska Julia <224441@edu.p.lodz.pl>
2020-10-19 10:08:22 +0200

    Added two buttons

--- Ex_5/app/src/main/java/tech/szymanskazdrzalik/youtubeplayer/MainActivity.java
+++ Ex_5/app/src/main/java/tech/szymanskazdrzalik/youtubeplayer/MainActivity.java
@@ -1,7 +1,10 @@
 package tech.szymanskazdrzalik.youtubeplayer;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
@@ -10,17 +13,11 @@ import com.google.android.youtube.player.YouTubePlayerView;
 
 public class MainActivity extends YouTubeBaseActivity {
 
+    private String[][] selectedFilms;
+    private int selected;
     private YouTubePlayerView youTubePlayerView;
-    private final YouTubePlayer.OnInitializedListener onInitializedListener = new YouTubePlayer.OnInitializedListener() {
-        @Override
-        public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-            youTubePlayer.loadVideo("s0-f5RncxcA");
-        }
-
-        @Override
-        public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-        }
-    };
+    private YouTubePlayer player;
+    private Boolean wasReleased = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +25,55 @@ public class MainActivity extends YouTubeBaseActivity {
         setContentView(R.layout.activity_main);
 
         youTubePlayerView = findViewById(R.id.youtube_view);
-
+        selectedFilms = new String[][]{new String[]{"s0-f5RncxcA", "n_1XpKHWMU0", "UWLr2va3hu0"}, new String[]{"Take you dancing", "Hey DJ", "Hey Ma"}};
+        selected = 0;
     }
 
-    public void playButtonListener(View v){
-        youTubePlayerView.initialize("", onInitializedListener);
+    private final YouTubePlayer.OnInitializedListener onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+        @Override
+        public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+            try {
+                youTubePlayer.loadVideo(selectedFilms[0][selected]);
+                player = youTubePlayer;
+                wasReleased = false;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        }
+
+    };
+
+    public void playButtonListener(View v) {
+        if (wasReleased) {
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+            alertDialog.setTitle("AlertDialog");
+            int checkedItem = 1;
+            alertDialog.setSingleChoiceItems(selectedFilms[1], checkedItem, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    selected = which;
+                    youTubePlayerView.initialize("", onInitializedListener);
+                    dialog.dismiss();
+                }
+            });
+            AlertDialog alert = alertDialog.create();
+            alert.setCanceledOnTouchOutside(false);
+            alert.show();
+        } else {
+            player.play();
+        }
     }
 
+    public void pauseButtonListener(View v) {
+        player.pause();
+    }
+
+    public void stopButtonListener(View v) {
+        player.release();
+        wasReleased = true;
+    }
 }
